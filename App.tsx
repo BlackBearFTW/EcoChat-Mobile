bd52df6d088a5adb4c371401d79eec411e5c21fb
Robin Mager <robinmager@home.nl>
2021-11-03 13:17:42 +0100

    Changed marker icon, added user location with permission

--- App.tsx
+++ App.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react';
 import MapView, {Marker} from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions, StatusBar } from 'react-native';
+
+import * as Location from 'expo-location';
 import {mapStyle} from "./assets/map-style.json";
+import {useEffect} from "react";
 
 export default function App() {
+
+    useEffect(() => {
+        (async () => {
+            await Location.requestForegroundPermissionsAsync();
+        })();
+    }, []);
 
 
   return (
@@ -11,6 +20,7 @@ export default function App() {
             style={styles.map}
             customMapStyle={mapStyle}
             showsUserLocation={true}
+            minZoomLevel={10}
             showsCompass={false}
             initialRegion={{
               latitude: 51.441643,
@@ -22,11 +32,11 @@ export default function App() {
         <Marker
             key={"1"}
             coordinate={{
-              latitude: 51.450851,
-              longitude: 5.480200
+              latitude: 51.44369794468786,
+              longitude: 5.47874608280784
             }}
             image={require("./assets/marker-icon.png")}
-            pinColor={"navy"}
+            pinColor={"green"}
         />
       </MapView>
   );
