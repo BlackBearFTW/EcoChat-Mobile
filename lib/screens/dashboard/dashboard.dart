ad3e24f24b296a8a7d8429a91c34897f212ce147
Vincent-Maximus <57707853+Vincent-Maximus@users.noreply.github.com>
2021-11-23 14:07:00 +0100

    added file structure (#5)
    * added file structure
    
    * defined files, defined routes: user homescreen, auth, and admin homescreen. added a button to the home screen, added auth screen, made preperations for the upcomming auth api
    
    * folder name changes
    
    * finished the file structure
    
    * final final filestructure

---
+++ lib/screens/dashboard/dashboard.dart
@@ -0,0 +1,25 @@
+// import 'package:ecochat_app/services/api_methods.dart';
+import 'package:flutter/material.dart';
+
+// import 'create_marker.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  // @override
+  // void initState() {
+  //   //  TODO Add functions..
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text("homeview Admin"),
+    );
+  }
+}
