b0d9baaeee44820e03836deddec44ace33dde686
Robin Mager <robinmager@home.nl>
2021-11-25 15:09:47 +0100

    added google maps and bottom sheet

--- lib/screens/homepage/homepage.dart
+++ lib/screens/homepage/homepage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -13,6 +13,7 @@ class _HomeViewState extends State<HomeView> {
   // void initState() {
   //   //  TODO: Get initial marker data
   // }
+  late GoogleMapController mapController;
 
   @override
   Widget build(BuildContext context) {
@@ -23,20 +24,39 @@ class _HomeViewState extends State<HomeView> {
         centerTitle: true,
         title: const Text(
           "ECOCHAT",
-          style: TextStyle(
-              fontWeight: FontWeight.bold
-          ),
+          style: TextStyle(fontWeight: FontWeight.bold),
         ),
         backgroundColor: const Color(0xff33835c),
       ),
+      body: const GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(50.94284, 5.921534),
+          zoom: 14.4746,
+        ),
+        zoomControlsEnabled: false,
+        myLocationButtonEnabled: false,
+        myLocationEnabled: true,
+        compassEnabled: false,
+     
+      ),
+      
       floatingActionButton: FloatingActionButton(
         onPressed: () => {
-        showModalBottomSheet(
-        context: context,
-        builder: (BuildContext context) => Container(
-        child: Center(child: Text('This is a modal bottom sheet')),
-        ))
-      },
+          // showModalBottomSheet(
+          //     barrierColor: Colors.white.withOpacity(0),
+          //     backgroundColor: Colors.red,
+          //     shape: const RoundedRectangleBorder(
+          //       borderRadius: BorderRadius.only(
+          //           topLeft: Radius.circular(16),
+          //           topRight: Radius.circular(16)),
+          //     ),
+          //     context: context,
+          //     builder: (BuildContext context) => SizedBox(
+          //           height: MediaQuery.of(context).size.height * 0.25,
+          //           child: const Center(
+          //               child: Text('This is a modal bottom sheet')),
+          //         ))
+        },
         backgroundColor: Colors.white,
         child: const Icon(
           Icons.my_location,
