00a0e6e14375c0a10e1493bddec7041e34f185ba
Robin Mager <robinmager@home.nl>
2021-11-25 16:34:12 +0100

    added myLocation permission

--- lib/screens/homepage/homepage.dart
+++ lib/screens/homepage/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -9,11 +10,42 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  // @override
-  // void initState() {
-  //   //  TODO: Get initial marker data
-  // }
+  final String _mapStyle =
+      '[{"featureType":"all","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"all","elementType":"labels","stylers":[{"visibility":"on"},{"saturation":"-100"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":0},{"color":"#FFFFFF"},{"lightness":0},{"visibility":"true"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#575757"},{"lightness":0}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#4d6059"}]},{"featureType":"landscape","elementType":"geometry.stroke","stylers":[{"color":"#4d6059"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"color":"#4d6059"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"lightness":21}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"color":"#4d6059"}]},{"featureType":"poi","elementType":"geometry.stroke","stylers":[{"color":"#4d6059"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#7f8d89"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#7f8d89"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"color":"#7f8d89"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#2b3638"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#2b3638"},{"lightness":17}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#24282b"}]},{"featureType":"water","elementType":"geometry.stroke","stylers":[{"color":"#24282b"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.icon","stylers":[{"visibility":"off"}]}]';
   late GoogleMapController mapController;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async{
+    Location location = Location();
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _locationData = await location.getLocation();
+    print(_locationData);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,8 +60,8 @@ class _HomeViewState extends State<HomeView> {
         ),
         backgroundColor: const Color(0xff33835c),
       ),
-      body: const GoogleMap(
-        initialCameraPosition: CameraPosition(
+      body: GoogleMap(
+        initialCameraPosition: const CameraPosition(
           target: LatLng(50.94284, 5.921534),
           zoom: 14.4746,
         ),
@@ -37,25 +69,27 @@ class _HomeViewState extends State<HomeView> {
         myLocationButtonEnabled: false,
         myLocationEnabled: true,
         compassEnabled: false,
-     
+        onMapCreated: (GoogleMapController controller) {
+          mapController = controller;
+          controller.setMapStyle(_mapStyle);
+        },
       ),
-      
+
       floatingActionButton: FloatingActionButton(
         onPressed: () => {
-          // showModalBottomSheet(
-          //     barrierColor: Colors.white.withOpacity(0),
-          //     backgroundColor: Colors.red,
-          //     shape: const RoundedRectangleBorder(
-          //       borderRadius: BorderRadius.only(
-          //           topLeft: Radius.circular(16),
-          //           topRight: Radius.circular(16)),
-          //     ),
-          //     context: context,
-          //     builder: (BuildContext context) => SizedBox(
-          //           height: MediaQuery.of(context).size.height * 0.25,
-          //           child: const Center(
-          //               child: Text('This is a modal bottom sheet')),
-          //         ))
+          showModalBottomSheet(
+              barrierColor: Colors.white.withOpacity(0),
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16),
+                    topRight: Radius.circular(16)),
+              ),
+              context: context,
+              builder: (BuildContext context) => SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.25,
+                    child: const Center(
+                        child: Text('This is a modal bottom sheet')),
+                  ))
         },
         backgroundColor: Colors.white,
         child: const Icon(
@@ -66,3 +100,18 @@ class _HomeViewState extends State<HomeView> {
     );
   }
 }
+
+// showModalBottomSheet(
+//     barrierColor: Colors.white.withOpacity(0),
+//     backgroundColor: Colors.red,
+//     shape: const RoundedRectangleBorder(
+//       borderRadius: BorderRadius.only(
+//           topLeft: Radius.circular(16),
+//           topRight: Radius.circular(16)),
+//     ),
+//     context: context,
+//     builder: (BuildContext context) => SizedBox(
+//           height: MediaQuery.of(context).size.height * 0.25,
+//           child: const Center(
+//               child: Text('This is a modal bottom sheet')),
+//         ))
