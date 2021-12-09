bcb7af6d4252939e234913acef768bc71267e682
Robin Mager <robinmager@home.nl>
2021-12-09 13:28:56 +0100

    Added custom marker icon and changed map style

--- lib/screens/homepage/homepage.dart
+++ lib/screens/homepage/homepage.dart
@@ -12,36 +12,51 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  GoogleMapController? _mapController;
-  BitmapDescriptor markerIcon = BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen);
-  final Set<Marker> _markers = {};
   final Location _locationHandler = Location();
   bool locationPermissionAllowed = false;
 
+  GoogleMapController? _mapController;
+  late BitmapDescriptor markerIcon = BitmapDescriptor.defaultMarker;
+  final Set<Marker> _markers = {};
+
+
   @override
   void initState() {
-    BitmapDescriptor.fromAssetImage(
-        const ImageConfiguration(devicePixelRatio: 2.5),
-        "assets/marker-icon.png"
-    ).then((value) => markerIcon = value);
-    _markers.add(_createMarker("avgre-43er", const LatLng(51.451555623652524, 5.480393955095925)));
+    _loadCustomMarkerIcon();
+
     _askForLocationPermission().then((value) => setState(() => locationPermissionAllowed = value));
     super.initState();
   }
 
-  Marker _createMarker(String id, LatLng coordinates) {
-
-    return Marker(
-        markerId: MarkerId(id),
-        position: coordinates,
-        icon: markerIcon
+  void _loadCustomMarkerIcon() async {
+    BitmapDescriptor _customMarker = await BitmapDescriptor.fromAssetImage(
+        const ImageConfiguration(devicePixelRatio: 2.5),
+        "assets/marker-icon.png"
     );
+
+    setState(()  {
+      markerIcon = _customMarker;
+    });
   }
+
+  Marker _createMarker(String id, LatLng coordinates) => Marker(
+      markerId: MarkerId(id),
+      position: coordinates,
+      icon: markerIcon
+  );
 
   void _onGoogleMapLoad(GoogleMapController controller) async {
     _mapController = controller;
     String _mapStyle = await rootBundle.loadString('assets/map_style.txt');
-    controller.setMapStyle(_mapStyle);
+    _mapController?.setMapStyle(_mapStyle);
+
+    setState(() {
+      _markers.add(_createMarker("avgre-43er", const LatLng(51.451555623652524, 5.480393955095925)));
+    });
+
+    if (!locationPermissionAllowed) return;
+    LocationData locationData = await _locationHandler.getLocation();
+    _mapController?.moveCamera(CameraUpdate.newLatLngZoom(LatLng(locationData.latitude!, locationData.longitude!), 18));
   }
 
   Future<bool> _askForLocationPermission() async {
@@ -60,7 +75,7 @@ class _HomeViewState extends State<HomeView> {
       appBar: AppBar(
         centerTitle: true,
         title: const Text("ECOCHAT", style: TextStyle(fontWeight: FontWeight.bold)),
-        backgroundColor: const Color(0xff33835c),
+        backgroundColor: const Color(0xff7672FF),
       ),
       body: GoogleMap(
         initialCameraPosition: const CameraPosition(target: LatLng(51, 5), zoom: 14.4746),
@@ -72,6 +87,7 @@ class _HomeViewState extends State<HomeView> {
         tiltGesturesEnabled: false,
         onMapCreated: _onGoogleMapLoad,
         markers: _markers,
+        mapToolbarEnabled: false,
       ),
       floatingActionButton: _mapController != null ? MyLocationButton(
         disabled: locationPermissionAllowed,
