0d80b2843be2e037e0f1f463262f195161ca586c
Robin Mager <62018777+BlackBearFTW@users.noreply.github.com>
2021-12-21 12:31:51 +0100

    Homepage screen, including popup, routes en live data
    * added appbar and bottom sheet
    
    * added google maps and bottom sheet
    
    * added myLocation permission
    
    * added styling to appBar and *trying* to read text file for styling
    
    * cleanup still WIP
    
    * added myLocation button widget
    
    * Now forces portrait mode (otherwise map gets weird) and changed zoom level
    
    * removed old files, made floating action nullable, now read text file for map style
    
    * Added markers, tried giving a custom image
    
    * Added custom marker icon and changed map style
    
    * Update marker-icon.png
    
    * added utils for image
    
    * added initial popup
    
    * DI signalRMarkers into MarkerPopup and some debugging
    
    * Made SignalR class use streams instead of callback, and fixed the popup
    
    * Added some styling to popup and map now gets dynamic markers
    
    * Fixed infinite rendering loop bug, by using a completer. also made sure user leaves group on server side.
    
    * Added name to markermodel
    
    * Changed popup UI styling and structure, added name at top of popup. Lastly added skeleton loading animation to popup
    
    * added routing using openrouteservice
    
    * Temporarly disabled travel distance calculator and button now changes when route active, but have to rerender widget
    
    * Changed to geolocator package, because location doesn't work and now turned travelTime into stream. Also added alert when location is disabled and changed to bottom sheet
    
    * FAB now shows up above bottomSheet

--- lib/models/marker_model.dart
+++ lib/models/marker_model.dart
@@ -1,5 +1,6 @@
 class MarkerModel {
   late String id;
+  late String name;
   late bool roofed;
   late double latitude;
   late double longitude;
@@ -7,8 +8,11 @@ class MarkerModel {
   late int availableSlots;
   late int totalSlots;
 
+  MarkerModel(this.id, this.name, this.roofed, this.latitude, this.longitude, this.batteryLevel, this.availableSlots, this.totalSlots);
+
   MarkerModel.fromJson(Map<String, dynamic> json) {
     id = json['id'];
+    name = json['name'];
     roofed = json['roofed'];
     latitude = json['latitude'];
     longitude = json['longitude'];
