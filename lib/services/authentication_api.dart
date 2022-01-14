2b69a64ab12060d49bd7cc87513e8ad68d568074
Vincent-Maximus <57707853+Vincent-Maximus@users.noreply.github.com>
2022-01-14 13:09:36 +0100

    Implemented Wrapper for both Markers Endpoint and for Authorization Endpoint
    * added file structure
    
    * defined files, defined routes: user homescreen, auth, and admin homescreen. added a button to the home screen, added auth screen, made preperations for the upcomming auth api
    
    * folder name changes
    
    * finished the file structure
    
    * final final filestructure
    
    * made SignalR implementation call and recieve individual and all marker(s) data, deadline push
    
    * NOT woroking - added OOP, Models, Services, Classes
    
    * made connection via simple method, not OOP way
    
    * now working with OOP
    
    * latested code (to fix)
    
    * recieve data, and push it into a object, read out the object
    
    * showing signalR data on the front end
    
    * refactored signal r, removed unused functions and moved main.dart
    
    * applied requested comments, didnt fix conflicts yet
    
    * first commit- unfished
    
    * fixed errors
    
    * huidige stand van zaken
    
    * huidige stand van zaken
    
    * basic form now complete, and other simple fixes
    
    * Revert "basic form now complete, and other simple fixes"
    
    This reverts commit 5173a2b4c17dd913ac1d7ee39d2740cd5e8fa815.
    
    * resolved branches issue
    
    * fixing markers_api splitting auth
    
    * fixed markers_api Crud
    
    * added nullable get
    
    * Added return type to getMarker
    
    * Fixed merge conflicts
    
    * Delete dashboard.dart
    
    Co-authored-by: Robin Mager <robinmager@home.nl>
    Co-authored-by: Robin Mager <62018777+BlackBearFTW@users.noreply.github.com>

---
+++ lib/services/authentication_api.dart
@@ -0,0 +1,15 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class AuthenticationApi {
+  String serverUrlAuth = "https://i496018core.venus.fhict.nl/api/authentication";
+
+  Future<String> login(String username, String password) async {
+    String basicAuth = 'Basic ' + base64Encode(utf8.encode('$username:$password'));
+    http.Response response = await http.post(
+      Uri.parse(serverUrlAuth + "/login"),
+      headers: {'authorization': basicAuth},
+    );
+    return response.body.toString();
+  }
+}
